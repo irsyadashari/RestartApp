b32a2caa971fa3619ed26872e14e4f1da29fce3c
Irsyad Ashari <muh.ashari@gojek.com>
2024-08-30 10:58:28 +0700

    Adding Init project

---
+++ RestartApp/RestartApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestartApp
+//
+//  Created by Muh Irsyad Ashari on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Muh Irsyad Ashari on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
